5ca3d32fbdc47ca8f99e815e1c65658f99c1a4f9
Narut Poovorakit <net.narut.p@gmail.com>
2017-05-13 19:48:28 +0700

    Edit InformationUI for user to input information

--- src/application/InformationUI.java
+++ src/application/InformationUI.java
@@ -1,8 +1,18 @@
 package application;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import user.User;
 
 public class InformationUI extends JFrame implements Runnable {
 
@@ -10,7 +20,11 @@ public class InformationUI extends JFrame implements Runnable {
 	private int age;
 
 	private JLabel ageLabel, nameLabel, genderLabel, activityLabel;
-	private JTextField nameTxt, ageTxt, genderTxt, activityTxt;
+	private JTextField nameTxt, ageTxt;
+	private JComboBox<String> genderBox, activityBox;
+	private JButton enterBtn;
+	
+	private User user;
 	
 	public InformationUI() {
 		this.setTitle("Basic Information");
@@ -19,7 +33,60 @@ public class InformationUI extends JFrame implements Runnable {
 	}
 	
 	public void initComponents() {
+		ageLabel = new JLabel("Age: ");
+		nameLabel = new JLabel("Name: ");
+		genderLabel = new JLabel("Gender: ");
+		activityLabel = new JLabel("Activity: ");
+		nameTxt = new JTextField(10);
+		ageTxt = new JTextField(5);
+		enterBtn = new JButton("ENTER");
 		
+		String[] genderArr = {"Male", "Female"};
+		String[] activityArr = {"Sedentary", "Moderately Active", "Active"};
+		
+		genderBox = new JComboBox(genderArr);
+		activityBox = new JComboBox(activityArr);
+		
+		enterBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				name = nameTxt.getText();
+				age = Integer.parseInt(ageTxt.getText());
+				gender = genderBox.getSelectedItem() + "";
+				activity = activityBox.getSelectedItem() + "";
+				
+				//Create User
+				user = new User(name);
+				user.setActivity(activity);
+				user.setGender(gender);
+				user.setAge(age);
+			}
+		});
+		
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		JPanel panel5 = new JPanel();
+		JPanel bigPanel = new JPanel();
+		
+		panel1.add(nameLabel);
+		panel1.add(nameTxt);
+		panel2.add(ageLabel);
+		panel2.add(ageTxt);
+		panel3.add(genderLabel);
+		panel3.add(genderBox);
+		panel4.add(activityLabel);
+		panel4.add(activityBox);
+		panel5.add(enterBtn);
+		
+		bigPanel.setLayout(new BoxLayout(bigPanel, BoxLayout.Y_AXIS));
+		bigPanel.add(panel1);
+		bigPanel.add(panel2);
+		bigPanel.add(panel3);
+		bigPanel.add(panel4);
+		bigPanel.add(panel5);
+		this.add(bigPanel);
 	}
 	
 	@Override
--- src/application/Main.java
+++ src/application/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		StartUI startPage = new StartUI();
 		startPage.run();
+		
 	}
 }
