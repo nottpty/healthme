47e143616b3814f41f3efc8c18e1907d75c5f657
patinya yongyai <patinya.y@ku.th>
2017-05-14 23:44:31 +0700

    Add getMortalityRisk() method in Absi class

--- src/absi/Absi.java
+++ src/absi/Absi.java
@@ -1,6 +1,8 @@
 package absi;
 
 /**
+ * A Body Shape Index (ABSI) calculator takes your weight, height and waist
+ * circumference and calculates the ABSI value.
  * 
  * @author Patinya Yongyai
  *
@@ -9,20 +11,36 @@ public class Absi {
 	private double height, weight, waist_circum;
 	private int age;
 	private String gender;
-	// A risk categories that categorized by absi score.
-	private String riskCategory;
-	
-	public Absi(String gender, int age, double height, double weight, double waist_circum) {
+
+	public void setInfo(String gender, int age, double height, double weight, double waist_circum) {
 		this.gender = gender;
 		this.age = age;
 		this.height = height;
 		this.weight = weight;
 		this.waist_circum = waist_circum;
 	}
-	
-	public double getAbsiValue() {
-		double bmi = weight/Math.pow((height/100), 2);
-		double absiValue = waist_circum/(Math.cbrt(Math.pow(bmi, 2)) * Math.sqrt(height));
+
+	public double getValue() {
+		double bmi = weight / Math.pow((height / 100), 2);
+		double absiValue = waist_circum / (Math.cbrt(Math.pow(bmi, 2)) * Math.sqrt(height));
 		return absiValue;
+	}
+
+	public String getMortalityRisk() {
+		if (getZscore() < -0.868)
+			return "Very Low";
+		else if (getZscore() >= -0.868 && getZscore() <= -0.272)
+			return "Low";
+		else if (getZscore() >= -0.272 && getZscore() <= 0.229)
+			return "Average";
+		else if (getZscore() >= 0.229 && getZscore() <= 0.789)
+			return "High";
+		return "Very High";
+	}
+
+	public double getZscore() {
+		// read the data from a text file in local storage
+		// male and female are not same result.
+		return 0.0;
 	}
 }
