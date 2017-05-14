a6bce7690bc14188f4225f12cc2334b587f096fc
patinya yongyai <patinya.y@ku.th>
2017-05-14 18:32:48 +0700

    add getAbsiValue() method

--- src/absi/Absi.java
+++ src/absi/Absi.java
@@ -1,9 +1,28 @@
 package absi;
 
+/**
+ * 
+ * @author Patinya Yongyai
+ *
+ */
 public class Absi {
-	
-	private double absiValue, absiScore;
-	
+	private double height, weight, waist_circum;
+	private int age;
+	private String gender;
 	// A risk categories that categorized by absi score.
 	private String riskCategory;
+	
+	public Absi(String gender, int age, double height, double weight, double waist_circum) {
+		this.gender = gender;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		this.waist_circum = waist_circum;
+	}
+	
+	public double getAbsiValue() {
+		double bmi = weight/Math.pow((height/100), 2);
+		double absiValue = waist_circum/(Math.cbrt(Math.pow(bmi, 2)) * Math.sqrt(height));
+		return absiValue;
+	}
 }
